186e202658f66974112baad901de16a7215497d5
Пырцак Герман FO1ULI <rbebi96@mail.ru>
2026-02-23 12:30:38 +0700

    Выполнил пункт 2.4. Cортировка

---
+++ RbacProj/src/main/java/com/rbac/sorter/AssignmentSorters.java
@@ -0,0 +1,18 @@
+package com.rbac.sorter;
+
+import com.rbac.model.RoleAssignment;
+import java.util.Comparator;
+
+public class AssignmentSorters {
+    public static Comparator<RoleAssignment> byUsername() {
+        return Comparator.comparing(a -> a.user().username());
+    }
+
+    public static Comparator<RoleAssignment> byRoleName() {
+        return Comparator.comparing(a -> a.role().getName());
+    }
+
+    public static Comparator<RoleAssignment> byAssignmentDate() {
+        return Comparator.comparing(a -> a.metadata().assignedAt());
+    }
+}
